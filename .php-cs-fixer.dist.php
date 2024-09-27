3a46060dcc249ec6d5f1978d76dc45d67c2b9fe9
Artem Yeremenko <a.yeremenko@asbis.com>
2024-09-27 15:03:13 +0300

    add cs fixer config

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+$finder = (new PhpCsFixer\Finder())
+    ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        '@PHP83Migration' => true,
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+    ;
